2efac47e484128f6ae6af02ba3f0940a2349039f
Sofia Bargues <barguessofia@gmail.com>
2024-01-01 23:05:20 +0000

    app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Route,  Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Memotest from "./Memotest";
 import WordsPerMinute from "./WordsPerMinute";
 import Pokemon from "./Pokemon";
 
 function App() {
-
   return (
     <Routes>
-      <Route element={<Memotest/> } path="/memotest" />
-      <Route element={<WordsPerMinute/> } path="/wpm" />
-      <Route element={<Pokemon/> } path="/pokemon" />
+      <Route path="/" />
+      <Route element={<Memotest />} path="/memotest" />
+      <Route element={<WordsPerMinute />} path="/wpm" />
+      <Route element={<Pokemon />} path="/pokemon" />
     </Routes>
-    )
+  );
 }
 
-export default App
+export default App;
